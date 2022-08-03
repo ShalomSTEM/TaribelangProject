bad9298ba4dc017a6ad2978432a43b679a800a08
Samuel Heinz <sam@samheinz.com>
2022-08-03 15:31:51 +1000

    code from website in description
    https://programmingpixels.com/handling-a-title-screen-game-flow-and-buttons-in-pygame.html

--- MainController.py
+++ MainController.py
@@ -45,3 +45,48 @@ class UIElement(Sprite):
 
         # calls the init method of the parent sprite class
         super().__init__()
+    # properties that vary the image and its rect when the mouse is over the element
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+
+    @property
+    def rect(self):
+        return self.rects[1] if self.mouse_over else self.rects[0]
+    def update(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+        else:
+            self.mouse_over = False
+
+    def draw(self, surface):
+        """ Draws element onto a surface """
+        surface.blit(self.image, self.rect)
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600))
+
+    # create a ui element
+    uielement = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Start Game",
+    )
+
+    # main loop
+    while True:
+        for event in pygame.event.get():
+            pass
+        screen.fill(BLUE)
+
+        uielement.update(pygame.mouse.get_pos())
+        uielement.draw(screen)
+        pygame.display.flip()
+
+
+# call main when the script is run
+if __name__ == "__main__":
+    main()
