3f7988fd4561e975643b59f283c6aef2b8fc76c4
Sam Heinz <sam@samheinz.com>
2022-08-05 18:41:50 +1000

    Update MainController.py

--- MainController.py
+++ MainController.py
@@ -3,6 +3,7 @@ import pygame.freetype
 from pygame.sprite import Sprite
 from pygame.rect import Rect
 from enum import Enum
+from pygame.sprite import RenderUpdates
 
 BLUE = (106, 159, 181)
 WHITE = (255, 255, 255)
@@ -39,12 +40,12 @@ class UIElement(Sprite):
         )
 
         self.images = [default_image, highlighted_image]
+
         self.rects = [
             default_image.get_rect(center=center_position),
             highlighted_image.get_rect(center=center_position),
         ]
 
-        # assign button action
         self.action = action
 
         super().__init__()
@@ -73,11 +74,47 @@ class UIElement(Sprite):
         surface.blit(self.image, self.rect)
 
 
+class Player:
+    """ Stores information about a player """
+
+    def __init__(self, score=0, lives=3, current_level=1):
+        self.score = score
+        self.lives = lives
+        self.current_level = current_level
+
+
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((800, 600))
+    game_state = GameState.TITLE
 
+    while True:
+        if game_state == GameState.TITLE:
+            game_state = title_screen(screen)
+
+        if game_state == GameState.NEWGAME:
+            player = Player()
+            game_state = play_level(screen, player)
+
+        if game_state == GameState.NEXT_LEVEL:
+            player.current_level += 1
+            game_state = play_level(screen, player)
+
+        if game_state == GameState.QUIT:
+            pygame.quit()
+            return
+
+
+def title_screen(screen):
+    start_btn = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Start",
+        action=GameState.NEWGAME,
+    )
     quit_btn = UIElement(
         center_position=(400, 500),
         font_size=30,
@@ -87,7 +124,39 @@ def main():
         action=GameState.QUIT,
     )
 
-    # main loop
+    buttons = RenderUpdates(start_btn, quit_btn)
+
+    return game_loop(screen, buttons)
+
+
+def play_level(screen, player):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Return to main menu",
+        action=GameState.TITLE,
+    )
+
+    nextlevel_btn = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text=f"Next level ({player.current_level + 1})",
+        action=GameState.NEXT_LEVEL,
+    )
+
+    buttons = RenderUpdates(return_btn, nextlevel_btn)
+
+    return game_loop(screen, buttons)
+
+
+def game_loop(screen, buttons):
+    """ Handles game loop until an action is return by a button in the
+        buttons sprite renderer.
+    """
     while True:
         mouse_up = False
         for event in pygame.event.get():
@@ -95,15 +164,20 @@ def main():
                 mouse_up = True
         screen.fill(BLUE)
 
-        ui_action = quit_btn.update(pygame.mouse.get_pos(), mouse_up)
-        if ui_action is not None:
-            return
-        quit_btn.draw(screen)
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+
+        buttons.draw(screen)
         pygame.display.flip()
 
 
 class GameState(Enum):
     QUIT = -1
+    TITLE = 0
+    NEWGAME = 1
+    NEXT_LEVEL = 2
 
 
 if __name__ == "__main__":
