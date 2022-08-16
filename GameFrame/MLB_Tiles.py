818326604aabfeaf840f2e866f1e1205cc2dd4ac
Sam Heinz <sam@samheinz.com>
2022-08-17 09:38:51 +1000

    summary

--- GameFrame/MLB_Tiles.py
+++ GameFrame/MLB_Tiles.py
@@ -7,13 +7,13 @@ from Objects import Grass, Dirt, Player, WaterIcon
 from Objects.WaterIcon_Flash import WaterIcon_Flash
 
 
-class Tiles(Level):
+class MilbiL1(Level):
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
         self.TileSize = 100
-        self.Center = 0
         self.map = []
         self.Tilemap = []
+        self.AnimalTileMap = []
         self.mapsize = 100
         self.Width_TileNum = int(Globals.SCREEN_WIDTH / self.TileSize)
         self.Height_TileNum = int(Globals.SCREEN_HEIGHT / self.TileSize)
@@ -25,18 +25,18 @@ class Tiles(Level):
         self.add_room_object(
             Player(
                 self,
-                Globals.SCREEN_WIDTH / 2
-                - self.TileSize / 2
-                - (Globals.SCREEN_WIDTH % self.TileSize) / 2,
+                Globals.SCREEN_WIDTH / 2 - self.TileSize / 2,
                 Globals.SCREEN_HEIGHT / 2 - self.TileSize / 2,
                 self.TileSize,
             )
         )
         self.Eaten = False
-        self.add_room_object(WaterIcon(self, Globals.SCREEN_WIDTH - 50, 20))
+        self.watericon = WaterIcon(self, Globals.SCREEN_WIDTH - 50, 20)
+        self.add_room_object(self.watericon)
         # self.waterFlash=WaterIcon_Flash(self,Globals.SCREEN_WIDTH-50,20)
         # self.add_room_object(self.waterFlash)
         self.UpdateWorld()
+        self.AnimalMove()
 
     def UpdateWorld(self):
         self.set_timer(2, self.UpdateWorld)
@@ -44,7 +44,6 @@ class Tiles(Level):
         if (
             self.prev_player_x != Globals.player_x
             or self.prev_player_y != Globals.player_y
-            or self.Eaten
         ):
             print("changed")
             if Globals.player_x > self.prev_player_x:
@@ -59,13 +58,16 @@ class Tiles(Level):
             Globals.player_y = self.prev_player_y
             self.ChangeVisTileMap()
             self.DisplayTilemap()
-            print(Globals.player_x),
+            print(Globals.player_x)
         """
         if (Globals.lowWater):
             self.waterFlash.flash()
             self.delete_object(self.waterFlash)
             self.waterFlash=WaterIcon_Flash(self,Globals.SCREEN_WIDTH-50,20)
         """
+
+    def AnimalMove(self):
+        self.set_timer(30, self.AnimalMove)
 
     def ChangeVisTileMap(self):
         self.VisTileMap = []
@@ -76,6 +78,13 @@ class Tiles(Level):
             self.VisTileMap.append(row)
         print(self.VisTileMap)
         # self.Center=self.VisTileMap[]
+        if self.VisTileMap[int(self.Width_TileNum / 2)][int(self.Height_TileNum / 2)]:
+            self.Eaten = True
+            self.watericon.water_level = min(self.watericon.water_level + 1, 16)
+        self.VisTileMap[int(self.Width_TileNum / 2)][int(self.Height_TileNum / 2)] = 0
+        self.Tilemap[int(self.Width_TileNum / 2) + Globals.player_x][
+            int(self.Height_TileNum / 2) + Globals.player_y
+        ] = 0
 
     def InitializeTileMap(self):
         # Map Gen Algorithm
@@ -126,3 +135,13 @@ class Tiles(Level):
         # Delete from array
         for i in range(self.Width_TileNum):
             self.map.pop(0)
+
+    def InitializeAnimalMap(self):
+        for i in range(self.mapsize):
+            row = []
+            for j in range(self.mapsize):
+                if random.randint(0, 10) == 0:
+                    row.append(1)
+                else:
+                    row.append(0)
+            self.AnimalTileMap.append(row)
