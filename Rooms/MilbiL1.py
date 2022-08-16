befd3d50b9891cbcf0ba4f02fa8666141546eaaa
Cjenk88 <111263281+Cjenk88@users.noreply.github.com>
2022-08-17 09:20:01 +1000

    change files

--- Rooms/MilbiL1.py
+++ Rooms/MilbiL1.py
@@ -4,5 +4,5 @@ from GameFrame import Globals, Tiles
 class MilbiL1(Tiles):
     def __init__(self, screen, joysticks):
         Tiles.__init__(self, screen, joysticks)
-        pass
+        print("test")
 
