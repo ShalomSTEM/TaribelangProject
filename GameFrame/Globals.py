43c883fb077e1bcf9d273dbf8d6bba43e7c4866f
Sam Heinz <sam@samheinz.com>
2022-08-17 09:42:46 +1000

    Update Globals.py

--- GameFrame/Globals.py
+++ GameFrame/Globals.py
@@ -1,3 +1,33 @@
+from enum import IntEnum
+
+
+class EnumLevels(IntEnum):
+    TitleRoom = 0
+    InfoPage = 1
+    Controls = 2
+    About = 3
+    WurrumbuPage = 4
+    ManjalPage = 5
+    KubirriPage = 6
+    QuizPage = 7
+    FinishQuiz = 8
+    KubirriAndWurrumbuPage = 9
+    StoryIntro = 10
+    StoryMid = 11
+    StoryWurrumbu = 12
+    StoryEnd = 13
+    ForagingRoom = 14
+    EscapeLevelIntro = 15
+    EscapeLevelInstructions = 16
+    EscapeLevel = 17
+    EscapeLevelOutro = 18
+    FightRoom = 19
+    StoryClimb = 20
+    DonkeyKong_Controls = 21
+    DonkeyKong = 22
+    LevelSelect = 23
+
+
 class Globals:
 
     running = True
@@ -17,7 +47,6 @@ class Globals:
 
     # - Set the order of the rooms - #
     levels = ["MilbiL1", "MilbiL3"]
-
 
     # - Set the starting level - #
     start_level = 0
@@ -40,8 +69,6 @@ class Globals:
     total_count = 0
     destroyed_count = 0
 
-
     player_x = 0
     player_y = 0
     lowWater = False
-
