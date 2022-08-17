61483773fbb88a8e53b4cac3c47ee89d1737071a
Sam Heinz <sam@samheinz.com>
2022-08-17 11:10:31 +1000

    merge (#80)
    * merge (#72)
    
    * L1 milbi (#63)
    
    * AAAA
    
    * AMONGUS
    
    * TiledScrolling
    
    * GenAlgChange+InitializeAtArbitraryCoords
    
    * Person
    
    * push
    
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    * Delete graphics
    
    * Delete .gitmodules
    
    * Delete .gitignore
    
    * Delete requirements.txt
    
    * Delete MainController.gameframe.py
    
    * Seperate tiling (#69)
    
    * asd
    
    * seperate MilbiL1 from the tiling system
    
    * Update .gitignore
    
    * remove pyc
    
    Co-authored-by: Sam Heinz <sam@samheinz.com>
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    * gdg
    
    * hjfgeiuyhdfhi
    
    * updating vcs
    
    * more chnages
    
    * merge (#74)
    
    * L1 milbi (#63)
    
    * AAAA
    
    * AMONGUS
    
    * TiledScrolling
    
    * GenAlgChange+InitializeAtArbitraryCoords
    
    * Person
    
    * push
    
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    * Delete graphics
    
    * Delete .gitmodules
    
    * Delete .gitignore
    
    * Delete requirements.txt
    
    * Delete MainController.gameframe.py
    
    * Seperate tiling (#69)
    
    * asd
    
    * seperate MilbiL1 from the tiling system
    
    * Update .gitignore
    
    * remove pyc
    
    Co-authored-by: Sam Heinz <sam@samheinz.com>
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    * refactord background
    
    * Fix (#76)
    
    * fix
    
    * summary
    
    * xml changes
    
    * Update Globals.py
    
    * Update Globals.py
    
    * b (#66) (#78)
    
    * b (#66)
    
    * Title screen (#62)
    
    * Update MainController.py
    
    * centring for title screen
    
    * centre new game, increase size of text
    
    * Update MainController.py
    
    Co-Authored-By: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update MainController.py
    
    * remove unnecessary dependencies
    
    * Create .gitignore
    
    * merge (#59)
    
    * testing new status checks (#54)
    
    * Update MainController.py
    * centring for title screen
    * centre new game, increase size of text
    
    * Create pylint-analysis.yml
    
    * Update pylint-analysis.yml
    
    * Create requirements.txt (#57)
    
    * Update pylint-analysis.yml (#58)
    
    * Rename pylint-analysis.yml to pylint-analysis.yml.stopplease
    
    * Sam (#56)
    
    * Remove lines from MainController.py
    
    removing some example lines for WIKI
    
    * .gitignore
    
    Co-Authored-By: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * update submodule
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update .gitignore
    
    * add milbi and carpet snake to title screen
    
    * add functionality
    
    still needs work, one button doesnt work
    
    * finalise milbi and carpet snake buttons
    
    both work
    
    * Create settings.json
    
    * add summaries
    
    * refactoring
    
    * move files
    
    * move maincontroller
    
    * Update MainController.py
    
    * fix errors
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update CONTRIBUTORS.md
    
    * L1 milbi (#63)
    
    * AAAA
    
    * AMONGUS
    
    * TiledScrolling
    
    * GenAlgChange+InitializeAtArbitraryCoords
    
    * Person
    
    * push
    
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    
    * fix
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    Co-authored-by: baileyr6 <reidb3@shalomcollege.com>
    
    * Delete MainController.gameframe.py
    
    * fix changes
    
    Co-authored-by: Will Coggins <110357970+srcoggin@users.noreply.github.com>
    Co-authored-by: Charlie D <dovec1@shalomcollege.com>
    Co-authored-by: Will Coggins <coggiw1@shalomcollege.com>
    Co-authored-by: ruby <TruemR1@shalomcollege.com>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: baileyr6 <reidb3@shalomcollege.com>

--- Objects/__init__.py
+++ Objects/__init__.py
@@ -3,4 +3,5 @@ from Objects.Dirt import Dirt
 from Objects.Player import Player
 from Objects.WaterIcon import WaterIcon
 from Objects.WaterIcon_Flash import WaterIcon_Flash
-
+from Objects.People import ML2_People
+from Objects.Elders import ML2_Elders
