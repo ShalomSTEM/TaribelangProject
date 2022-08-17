a28e6edd4f7ce9d6c73cb9dd7c14eaa29eadafee
Sam Heinz <sam@samheinz.com>
2022-08-17 11:08:02 +1000

    fix changes

--- Rooms/MilbiL1.py
+++ Rooms/MilbiL1.py
@@ -1,11 +1,7 @@
-import Tiles
+from GameFrame import Globals, MLB_Tiles, Tiles
 
 
-from GameFrame import Globals, MLB_Tiles
-
-
-class MilbiL1(MLB_Tiles):
+class MilbiL1(Tiles):
     def __init__(self, screen, joysticks):
-        MLB_Tiles.__init__(self, screen, joysticks)
+        Tiles.__init__(self, screen, joysticks)
         pass
-
