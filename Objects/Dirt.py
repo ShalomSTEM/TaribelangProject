862ca99b1e7a061d81be97c5a5e6819384305cff
Sam Heinz <sam@samheinz.com>
2022-08-19 14:26:41 +1000

    push

--- Objects/Dirt.py
+++ Objects/Dirt.py
@@ -3,4 +3,4 @@ from GameFrame import RoomObject
 class Dirt(RoomObject):
     def __init__(self,room,x,y,size):
         RoomObject.__init__(self,room,x,y)
-        self.set_image('Images/Brown.png',size,size)
+        self.set_image('MilbiL1/Brown.png',size,size)
