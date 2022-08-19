0687a73bf28a897f5a9533a581aefdd34952bf94
Charlie Dove <DoveC1@shalomcollege.com>
2022-08-19 14:11:03 +1000

    FIX-EVERYTHING

--- Rooms/MilbiL3.py
+++ Rooms/MilbiL3.py
@@ -1,7 +1,7 @@
-from GameFrame import Tiles
+from GameFrame import Level
 
 
-class MilbiL3(Tiles):
+class MilbiL3(Level):
     def __init__(self, screen, joysticks):
-        Tiles.__init__(self, screen, joysticks)
+        Level.__init__(self, screen, joysticks)
         self.set_background_image("PlaceHolderBackgroundMLBL3.png")
