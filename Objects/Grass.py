ad1c1c6fffcd4a112539cbc1835712bbc68f887e
Will Coggins <coggiw1@shalomcollege.com>
2022-08-19 14:34:42 +1000

    fggf

--- Objects/Grass.py
+++ Objects/Grass.py
@@ -4,4 +4,4 @@ from GameFrame import RoomObject
 class Grass(RoomObject):
     def __init__(self, room, x, y, size):
         RoomObject.__init__(self, room, x, y)
-        self.set_image("MilbiL1/Green.png", size, size)
+        self.set_image('Images/MilbiL1/Green.png', size, size)
