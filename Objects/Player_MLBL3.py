b7252aab636649ada4ff90c0e5ddcf46d04e0673
Sam Heinz <sam@samheinz.com>
2022-08-22 09:40:55 +1000

    merge  (#101)
    merge #101

--- Objects/Player_MLBL3.py
+++ Objects/Player_MLBL3.py
@@ -1,35 +1,68 @@
 import pygame
 from GameFrame import RoomObject, Globals
+import os
 
 
 class Player_MLBL3(RoomObject):
     def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
 
-        player = self.load_image('front_1.png')
+        player = self.load_image("front_1.png")
         self.set_image(player, 16, 24)
 
         # Load player animation images
         self.down = []
-        self.down.append(self.load_image('Images/MilbiL1/sprite_0.png'))
-        self.down.append(self.load_image('Images/MilbiL1/sprite_0.png'))
-        self.down.append(self.load_image('Images/MilbiL1/sprite_0.png'))
-        self.down.append(self.load_image('Images/MilbiL1/sprite_0.png'))
+        self.down.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_0.png"))
+        )
+        self.down.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_0.png"))
+        )
+        self.down.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_0.png"))
+        )
+        self.down.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_0.png"))
+        )
         self.up = []
-        self.up.append(self.load_image('Images/MilbiL1/sprite_2.png'))
-        self.up.append(self.load_image('Images/MilbiL1/sprite_2.png'))
-        self.up.append(self.load_image('Images/MilbiL1/sprite_2.png'))
-        self.up.append(self.load_image('Images/MilbiL1/sprite_2.png'))
+        self.up.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_2.png"))
+        )
+        self.up.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_2.png"))
+        )
+        self.up.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_2.png"))
+        )
+        self.up.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_2.png"))
+        )
         self.left = []
-        self.left.append(self.load_image('Images/MilbiL1/sprite_1.png'))
-        self.left.append(self.load_image('Images/MilbiL1/sprite_1.png'))
-        self.left.append(self.load_image('Images/MilbiL1/sprite_1.png'))
-        self.left.append(self.load_image('Images/MilbiL1/sprite_1.png'))
+        self.left.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_1.png"))
+        )
+        self.left.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_1.png"))
+        )
+        self.left.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_1.png"))
+        )
+        self.left.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_1.png"))
+        )
         self.right = []
-        self.right.append(self.load_image('Images/MilbiL1/sprite_3.png'))
-        self.right.append(self.load_image('Images/MilbiL1/sprite_3.png'))
-        self.right.append(self.load_image('Images/MilbiL1/sprite_3.png'))
-        self.right.append(self.load_image('Images/MilbiL1/sprite_3.png'))
+        self.right.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_3.png"))
+        )
+        self.right.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_3.png"))
+        )
+        self.right.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_3.png"))
+        )
+        self.right.append(
+            self.load_image(os.path.join(Globals.milbiL1_path, "sprite_3.png"))
+        )
 
         self.img_index = 0
 
@@ -42,7 +75,7 @@ class Player_MLBL3(RoomObject):
 
         self.handle_key_events = True
 
-        self.register_collision_object('Stone_MLBL3')
+        self.register_collision_object("Stone_MLBL3")
 
         self.block_right = False
         self.block_left = False
