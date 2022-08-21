a10a03ff0d829c434c4e5b13fbcec63f40aa2f78
Sam Heinz <sam@samheinz.com>
2022-08-22 09:40:19 +1000

    fix broken files

--- Objects/Stne_MLBL3.py
+++ Objects/Stne_MLBL3.py
@@ -1,10 +1,9 @@
-from GameFrame import RoomObject
+from GameFrame import RoomObject, Globals
+import os
 
 
 class Stne_MLBL3(RoomObject):
     def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
-
-        image = self.load_image('Images/MilbiL3/Stone_MLBL3.png')
+        image = self.load_image(os.path.join(Globals.milbiL3_path, "Stone_MLBL3.png"))
         self.set_image(image, 32, 32)
-
