4a631b8ee3dd4cff8a1aad583859e4bfafe6f38e
Sam Heinz <sam@samheinz.com>
2022-08-22 14:41:21 +1000

    create fullscreen overlay  (#103)
    create fullscreen overlay

--- GameFrame/StoryOverlayMaker.py
+++ GameFrame/StoryOverlayMaker.py
@@ -3,10 +3,19 @@ from Objects import StoryOverlay
 
 class Overlay:
     @staticmethod
-    def createOverlay(self, titleText, bodyText):
-        overlay = StoryOverlay.overlays(self, 0, 0)
-        title = StoryOverlay.Title(self, 0, 0, titleText)
-        body = StoryOverlay.Body(self, 0, 145, bodyText)
+    def createSmallOverlay(self, titleText, bodyText):
+        overlay = StoryOverlay.overlays(self, 0, 0, False)
+        title = StoryOverlay.Title(self, 0, 0, titleText, False)
+        body = StoryOverlay.Body(self, 0, 145, bodyText, False)
+        self.add_room_object(body)
+        self.add_room_object(title)
+        self.add_room_object(overlay)
+
+    @staticmethod
+    def createFullOverlay(self, titleText, bodyText):
+        overlay = StoryOverlay.overlays(self, 0, 0, True)
+        title = StoryOverlay.Title(self, 0, 0, titleText, True)
+        body = StoryOverlay.Body(self, 0, 145, bodyText, True)
         self.add_room_object(body)
         self.add_room_object(title)
         self.add_room_object(overlay)
--- Objects/StoryOverlay.py
+++ Objects/StoryOverlay.py
@@ -1,32 +1,55 @@
 from GameFrame import RoomObject, Globals
 import os
 
+stretch = False
+
 
 class overlays(RoomObject):
-    def __init__(self, room, x, y):
+    def __init__(self, room, x, y, stretch):
         RoomObject.__init__(self, room, x, y)
-        self.set_image(
-            os.path.join(Globals.storyOverlay_path, "StoryOverlay.png"),
-            width=Globals.SCREEN_WIDTH / 5,
-            height=720,
-        )
+        if stretch == False:
+            self.set_image(
+                os.path.join(Globals.storyOverlay_path, "StoryOverlay.png"),
+                width=Globals.SCREEN_WIDTH / 5,
+                height=Globals.SCREEN_HEIGHT,
+            )
+        else:
+            self.set_image(
+                os.path.join(Globals.storyOverlay_path, "StoryOverlay_stretched.png"),
+                width=Globals.SCREEN_WIDTH,
+                height=Globals.SCREEN_HEIGHT,
+            )
 
 
 class Title(RoomObject):
-    def __init__(self, room, x, y, image):
+    def __init__(self, room, x, y, image, stretch: bool):
         RoomObject.__init__(self, room, x, y)
-        self.set_image(
-            image,
-            width=Globals.SCREEN_WIDTH / 5.05,
-            height=Globals.SCREEN_HEIGHT / 5,
-        )
+        if stretch == False:
+            self.set_image(
+                image,
+                width=Globals.SCREEN_WIDTH / 5.05,
+                height=Globals.SCREEN_HEIGHT / 5,
+            )
+        else:
+            self.set_image(
+                image,
+                width=Globals.SCREEN_WIDTH,
+                height=Globals.SCREEN_HEIGHT / 5,
+            )
 
 
 class Body(RoomObject):
-    def __init__(self, room, x, y, image):
+    def __init__(self, room, x, y, image, stretch: bool):
         RoomObject.__init__(self, room, x, y)
-        self.set_image(
-            image,
-            width=Globals.SCREEN_WIDTH / 5.05,
-            height=(Globals.SCREEN_HEIGHT - (Globals.SCREEN_HEIGHT / 5.05)),
-        )
+        if stretch == False:
+            self.set_image(
+                image,
+                width=Globals.SCREEN_WIDTH / 5.05,
+                height=Globals.SCREEN_HEIGHT / 5,
+            )
+        else:
+            self.set_image(
+                image,
+                width=Globals.SCREEN_WIDTH,
+                height=Globals.SCREEN_HEIGHT - (Globals.SCREEN_HEIGHT / 5),
+            )
