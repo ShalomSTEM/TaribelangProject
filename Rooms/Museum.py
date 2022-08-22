7a2206ad831d3a7c98a7c6b23b6b7e166b852464
louieocar <ocarlouie.exe@gmail.com>
2022-08-23 08:55:20 +1000

    commit

--- Rooms/Museum.py
+++ Rooms/Museum.py
@@ -2,5 +2,9 @@ from GameFrame import Level
 
 
 class Museum(Level):
-    def __init__(self, screen, joystick):
-        Level.__init__()
+    def __init__(self, screen, joysticks):
+        Level.__init__(self, screen, joysticks)
+
+        self.set_background_image("")
+
+
