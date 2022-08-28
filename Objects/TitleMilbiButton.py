5a8e98e20e2f4e89ec9a1b44f470117b1e90157e
Sam Heinz <sam@samheinz.com>
2022-08-28 17:35:38 +1000

    please

--- Objects/TitleMilbiButton.py
+++ Objects/TitleMilbiButton.py
@@ -1,18 +1,20 @@
 from GameFrame import RoomObject, Globals
 import os
+import time
 
 
 class TitleMilbiButton(RoomObject):
     def __init__(self, room, x, y, level):
         RoomObject.__init__(self, room, x, y)
         self.set_image(
-            "Images/Title/button.png",
+            "Images/MilbiL1/Brown.png",
             height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
             width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
         )
         self.handle_key_events = True
-        update = updateTitle()
+        update = TitleMilbiButton.updateTitle(self)
 
+    @staticmethod
     def updateTitle(self):
         if Globals.title_Level == 0:
             self.set_image(
@@ -20,44 +22,52 @@ class TitleMilbiButton(RoomObject):
                 height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
                 width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
             )
+            time.sleep(0.5)
         elif Globals.title_Level == 1:
             self.set_image(
                 "Images/StoryOverlay/body_example.png",
                 height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
                 width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
             )
+            time.sleep(0.5)
         elif Globals.title_Level == 2:
             self.set_image(
                 "Images/StoryOverlay/title_example.png",
                 height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
                 width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
             )
+            time.sleep(0.5)
         elif Globals.title_Level == 3:
             self.set_image(
-                "Images/Title/button.png",
+                "Images/MilbiL1/Brown.png",
                 height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
                 width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
             )
+            time.sleep(0.5)
         elif Globals.title_Level == 4:
             self.set_image(
-                "Images/Title/button.png",
+                "Images/MilbiL1/Green.png",
                 height=Globals.SCREEN_HEIGHT - Globals.SCREEN_HEIGHT / 4,
                 width=Globals.SCREEN_WIDTH - Globals.SCREEN_WIDTH / 4,
             )
+            time.sleep(0.5)
         elif Globals.title_Level == -1:
             Globals.title_Level = 4
+            TitleMilbiButton.updateTitle(self)
+        elif Globals.title_Level == 5:
+            Globals.title_Level = 0
             TitleMilbiButton.updateTitle(self)
 
     def joy_pad_signal(self, p1_buttons, p2_buttons):
         if p1_buttons[11] > 0.5:
-            print("11 positive")
+            print("10 positive")
             Globals.title_Level -= 1
             TitleMilbiButton.updateTitle(self)
         if p1_buttons[11] < -0.5:
-            print("11 neg")
-            Globals.player_x += 1
+            print("10 neg")
+            Globals.title_Level += 1
             TitleMilbiButton.updateTitle(self)
         if p1_buttons[10] < -0.5:
-            print("10 neg")
+            print("11 neg")
         if p1_buttons[10] > 0.5:
-            print("10 pos")
+            print("11 pos")
