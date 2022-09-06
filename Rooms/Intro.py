a4276907d889fc6bff6445dbd6d52f7008e0637d
tuxta <tuxta2@gmail.com>
2022-09-07 09:35:47 +1000

    Added audio of Welcome to Country

--- Rooms/Intro.py
+++ Rooms/Intro.py
@@ -1,14 +1,23 @@
 from GameFrame import Level, TextObject
+import os
 
 
 class Intro(Level):
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
 
-        room_name = TextObject(self, 200, 300, "Intro - with WTC", colour="white")
-        self.add_room_object(room_name)
+        self.set_background_image(os.path.join("MilbiL3", "PlaceHolderBackground_MLBL3.png"))
+        self.set_timer(60, self.start_wtc_taribelang)
 
-        self.set_timer(60, self.complete)
+    def start_wtc_taribelang(self):
+        self.load_sound("WTC_Taribelang.ogg").play()
+        self.set_background_image(os.path.join("Title", "WTC_T.png"))
+        self.set_timer(840, self.start_wtc_english)
+
+    def start_wtc_english(self):
+        self.load_sound("WTC_English.ogg").play()
+        self.set_background_image(os.path.join("Title", "WTC_E.png"))
+        self.set_timer(1170, self.complete)
 
     def complete(self):
         self.running = False
