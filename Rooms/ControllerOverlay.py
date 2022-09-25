7cbd6ed1525c88928a200ee09a2d1492c92b3ff4
Sam Heinz <sam@samheinz.com>
2022-09-25 19:59:07 +1000

    Create a Press to Continue text (desc)
    Involves mainly the couple of lines at the bottom of Controller.py, which took me literal hours to figure out and many headaches.

--- Rooms/ControllerOverlay.py
+++ Rooms/ControllerOverlay.py
@@ -1,13 +1,11 @@
-from GameFrame import EnumLevels, Level, TextObject, Globals
-import os
-
+from GameFrame import Level, Globals
 from Objects import Controller2
-
 
 class ControllerOverlay(Level):
     def __init__(self, screen, joysticks):
         Level.__init__(self, screen, joysticks)
-        self.add_room_object(Controller2(self, 0, 0))
+        ControllerOverlays = Controller2(self, 0, 0)
+        self.add_room_object(ControllerOverlays)
     def complete(self):
         Globals.next_level = Globals.oldRoom
         self.running = False
