9ed5567a4795f837c5e918666e9a9843ddafbef5
Sam Heinz <sam@samheinz.com>
2022-10-09 16:39:16 +1000

    (MilbiG2) Add animations and variations to Tribes members

--- Objects/People.py
+++ Objects/People.py
@@ -4,29 +4,36 @@ from Objects.Dance_MLBL3 import DanceArrows_MLBL3, DanceBG_MLBL3, Dance_MLBL3
 
 
 class ML2_People(RoomObject):
-    def __init__(self, room, x, y, img):
+    def __init__(self, room, x, y, num):
         RoomObject.__init__(self, room, x, y)
-        #self.handle_mouse_events = True
+        self.num = num
         self.pos = 0
+        self.aniIndex = 1
         self.room.calcPos(x, y, self)
-        if self.x == 632 and self.y == 312:
-            self.yes = True
-            self.set_image(os.path.join("Images", "MilbiL1", "sprite_0.png"), 100, 100)
-        else:
-            self.yes = False
-            self.set_image(os.path.join("Images", "MilbiL2", img), height=100, width=100)
-        # print(self.x, self.y)
+        self.setnum()
+        
+    
+    def setnum(self):
+        if self.num == 1:
+            self.set_image(os.path.join("Images", "MilbiL2", "MLB2_1_1.png"), height=100, width=100)
+        if self.num == 2:
+            self.set_image(os.path.join("Images", "MilbiL2", "MLB2_2_1.png"), height=100, width=100)
+        if self.num == 3:
+            self.set_image(os.path.join("Images", "MilbiL2", "MLB2_3_1.png"), height=100, width=100)
+        if self.num == 4:
+            self.set_image(os.path.join("Images", "MilbiL2", "MLB2_4_1.png"), height=100, width=100)
+        if self.num == 5:
+            self.set_image(os.path.join("Images", "MilbiL2", "MLB2_5_1.png"), height=100, width=100)
         self.set_timer(40, self.updatePos)
-    #def mouse_event(self, mouse_x,mouse_y,button_left,button_middle,button_right):
-    #    print(mouse_x, mouse_y)
+        self.set_timer(10, self.animate)
+
     def updatePos(self):
-        self.pos += 1
-        if self.pos == 18:
-            self.pos = 0
-        if self.yes:
-            print(self.room.peoplePos[self.pos][0], self.room.peoplePos[self.pos][1], self.pos)
-        self.x = self.room.peoplePos[self.pos][0]
-        self.y = self.room.peoplePos[self.pos][1]
+        if self.room.startMoving:
+            self.pos += 1
+            if self.pos == 18:
+                self.pos = 0
+            self.x = self.room.peoplePos[self.pos][0]
+            self.y = self.room.peoplePos[self.pos][1]
         self.set_timer(40, self.updatePos)
     def update(self):
         self.y_speed = self.y_speed + self.gravity
@@ -37,3 +44,10 @@ class ML2_People(RoomObject):
         if self.y == 280 and self.x == 880:
             self.x = 100
             self.y = 100
+    def animate(self):
+        if self.room.startMoving:
+            self.set_image(os.path.join("Images", "MilbiL2", f"MLB2_{self.num}_{self.aniIndex}.png"), 100, 100)
+            self.aniIndex += 1
+            if self.aniIndex == 5:
+                self.aniIndex = 1
+        self.set_timer(10, self.animate)
