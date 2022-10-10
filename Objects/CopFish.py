6852a7ba16e0f22b9b5690a20cf93b01c7572450
Cjenk88 <111263281+Cjenk88@users.noreply.github.com>
2022-10-10 13:12:41 +1000

    added up and down images to lungfish

--- Objects/CopFish.py
+++ Objects/CopFish.py
@@ -8,10 +8,12 @@ class CopFish(RoomObject):
     def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
 
-        #self.img_up = self.load_image(os.path.join('CopG3', 'CopFish_Up.png'))
-        self.img_flat = self.load_image(os.path.join('CopG3', 'CopFish.png'))
-        #self.img_down = self.load_image(os.path.join('CopG3', 'CopFish_Down.png'))
+        self.img_up = self.load_image(os.path.join('CopG3', 'Up1.png'))
+        self.img_flat = self.load_image(os.path.join('CopG3', 'Straight1.png'))
+        self.img_down = self.load_image(os.path.join('CopG3', 'Down1.png'))
         self.set_image(self.img_flat, 65, 36)
+        self.set_image(self.img_down, 65, 36)
+        self.set_image(self.img_up, 65, 36)
 
         self.depth = 100
 
@@ -31,14 +33,17 @@ class CopFish(RoomObject):
         if key[pygame.K_UP]:
             if self.y > 50:
                 self.y -= 4
-                #self.set_image(self.img_up, 65, 36)
+                self.set_image(self.img_up, 65, 36)
+
         if key[pygame.K_DOWN]:
             if self.y < Globals.SCREEN_HEIGHT - 150:
                 self.y += 4
-                #self.set_image(self.img_down, 65, 36)
+                self.set_image(self.img_down, 65, 36)
+
         if key[pygame.K_RIGHT]:
             if self.x < Globals.SCREEN_WIDTH / 2 - 150:
                 self.x += 4
+
         if key[pygame.K_LEFT]:
             if self.x > 10:
                 self.x -= 4
@@ -47,13 +52,20 @@ class CopFish(RoomObject):
 
     def joy_pad_signal(self, p1_buttons, p2_buttons):
         if p1_buttons[11] < -0.5:
-            self.move_left()
+            if self.x > 10:
+                self.x -= 4
         elif p1_buttons[11] > 0.5:
-            self.move_right()
+            if self.x < Globals.SCREEN_WIDTH / 2 - 150:
+                self.x += 4
         if p1_buttons[10] < - 0.5:
-            self.move_up()
+            if self.y > 50:
+                self.y -= 4
+                self.set_image(self.img_up[0], 65, 36)
+                self.set_image(self.img_up[1], 65, 36)
         elif p1_buttons[10] > 0.5:
-            self.move_down()
+            if self.y < Globals.SCREEN_HEIGHT - 150:
+                self.y += 4
+                self.set_image(self.img_down, 65, 36)
 
     def handle_collision(self, other, other_type):
         self.room.remove_points()
