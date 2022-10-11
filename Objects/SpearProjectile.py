3c3548afbc51e9f0ef08973358145ab14f9b6700
Sam Heinz <sam@samheinz.com>
2022-10-11 20:41:45 +1000

    (MilbiG3) Simple health system

--- Objects/SpearProjectile.py
+++ Objects/SpearProjectile.py
@@ -7,10 +7,11 @@ from GameFrame import Globals
 class SpearProjectile(RoomObject):
     def __init__(self, room, x, y, milbi_boss):
         RoomObject.__init__(self, room, x, y)
-
+        self.milbiBoss = milbi_boss
+        self.countToDestory = 10
         image = self.load_image(os.path.join("MilbiL3", "Spear.png"))
         self.set_image(image, 12, 34)
-
+        
         # Register for collision with Enemy plane
         self.register_collision_object("BossMLBL3")
         self.register_collision_object("Stne_MLBL3")
@@ -22,12 +23,12 @@ class SpearProjectile(RoomObject):
     def handle_collision(self, other, other_type):
         if other_type == "BossMLBL3":
             Globals.destroyed_count += 1
-            if Globals.destroyed_count >= 10:
-                self.room.running = False
+            if Globals.destroyed_count >= self.countToDestory:
                 Globals.total_count = 0
                 Globals.destroyed_count = 0
-
-            self.room.delete_object(other)
+                self.room.delete_object(other)
+                self.room.running = False
+            print((self.countToDestory - Globals.destroyed_count)/10, '%')
             self.room.delete_object(self)
 
         elif other_type == "Stne_MLBL3":
