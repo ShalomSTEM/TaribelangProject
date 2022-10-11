80829d353080fcd051f3b7cdb23ad7cf91b7b405
ruby <TruemR1@shalomcollege.com>
2022-10-12 09:43:00 +1000

    cockatoo

--- Objects/Cockatoo.py
+++ Objects/Cockatoo.py
@@ -1,9 +1,11 @@
+import os
+
 from GameFrame import RoomObject
 
 
 class Cockatoo(RoomObject):
-    def __innit__(self, room, x, y):
+    def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
 
-        cockatoo_image = self.load_image('cockatoo.png')
+        cockatoo_image = self.load_image(os.path.join("CoppleS", "cockatoo.png"))
         self.set_image(cockatoo_image, 64, 64)
--- Rooms/Cop_S1.py
+++ Rooms/Cop_S1.py
@@ -11,10 +11,22 @@ class Cop_S1(Level):
         self.load_sound("Copple_1.wav").play()
         self.set_timer(900, self.complete)
 
-        Cockatoo_object = Cockatoo(self, 400, 300)
-        self.add_room_object(Cockatoo_object)
+        self.cockatoo_object = Cockatoo(self, -50, 200)
+        self.add_room_object(self.cockatoo_object)
+        self.cockatoo_object.x_speed = 5
+        self.cockatoo_object.y_speed = 1
+
+        self.set_timer(300, self.kill_bird)
+
+        self.set_timer(60, self.play_bird)
 
         Globals.next_level = EnumLevels.Cop_G1
+
+    def play_bird(self):
+        self.load_sound("BIRD_1.wav").play()
+
+    def kill_bird(self):
+        self.delete_object(self.cockatoo_object)
 
     def complete(self):
         self.running = False
