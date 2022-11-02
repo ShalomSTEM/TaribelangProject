1dd635018e8cfdd8e3a1edc3af46ba83764a2705
tuxta <8ribjxdgd>
2022-11-02 19:45:06 +1000

    Removed the Welcome to country audio.

--- Rooms/Intro.py
+++ Rooms/Intro.py
@@ -13,20 +13,18 @@ class Intro(Level):
 
         self.set_background_image(os.path.join("Title", "title_to_wtc.png"))
         self.add_room_object(ListenerIntro(self, 0, 0))
-        self.set_timer(60, self.start_wtc_taribelang)
+        self.set_timer(90, self.start_wtc_taribelang)
 
         Globals.change_select = self.load_sound("change_selection.ogg")
         Globals.confirm_select = self.load_sound("confirm_selection.ogg")
 
     def start_wtc_taribelang(self):
         self.set_background_image(os.path.join("Title", "WTC_T.png"))
-        self.load_sound("WTC_Taribelang.ogg").play()
-        self.set_timer(840, self.start_wtc_english)
+        self.set_timer(350, self.start_wtc_english)
 
     def start_wtc_english(self):
         self.set_background_image(os.path.join("Title", "WTC_E.png"))
-        self.load_sound("WTC_English.ogg").play()
-        self.set_timer(1170, self.complete)
+        self.set_timer(350, self.complete)
 
     def complete(self):
         self.running = False
